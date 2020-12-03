7f1bb28f9e230f27c4398b2520feaa05d9b6efe0
Nathan Silvera <nathan.s.untels@gmail.com>
2020-12-03 16:49:22 -0500

    Finalizado DAM-S2

---
+++ ConstraintLayout/app/src/main/java/com/dam/constraintlayout/MainActivity.java
@@ -0,0 +1,19 @@
+package com.dam.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+/*
+<color name="teal_200">#FF03DAC5</color>
+    <color name="teal_700">#FF018786</color>
+    <color name="black">#FF000000</color>
+    <color name="white">#FFFFFFFF</color>
+ */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
